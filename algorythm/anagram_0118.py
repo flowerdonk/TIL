db0df216a3343f733c5f0e7d06632bf8d121324d
flowerdonk <dbwls5140@gmail.com>
2023-01-18 14:19:58 +0900

    끝말잇기 함수

---
+++ algorythm/anagram_0118.py
@@ -0,0 +1,8 @@
+def group_anagrams():
+    anas = list(input().split(','))
+    result = []
+
+    return result
+
+
+print(group_anagrams())
--- algorythm/word_0117.py
+++ algorythm/word_0117.py
@@ -37,3 +37,44 @@ while idx < word_idx:
     elif idx == word_idx: # 만약 아무도 탈락하지 않고 끝까지 진행
         print('아무도 탈락하지 않았다.')
 
+
+# 함수로 작성
+
+def word_relay():
+    """
+    끝말잇기 게임 함수
+    함수 호출 시 게임이 시작된다
+    사용자가 'done'을 입력하거나, 중복될 때 함수 종료
+    """
+    word_list = []
+    idx = 1
+    is_playing = True
+    
+    while is_playing:
+        word = input('단어를 입력해주세요: ')
+
+        # 입력받은 단어가 'done'이면 게임 종료
+        if word == 'done':
+            print('사용자에 의해 게임이 임의로 종료되었습니다.')
+            is_playing = False
+
+        # 첫 번째 단어가 입력됐을 때 그냥 단어 추가
+        if not len(word_list):
+            word_list.append(word)
+            continue
+
+        word_list.append(word)
+
+        # 탈락자를 가려내는 조건
+        if words[idx - 1][-1] != words[idx][0] or words[idx] in words[:idx]:
+            print(f'{idx + 1}번째 참가자가 탈락')
+            is_playing = False
+
+        else: 
+            idx += 1
+            continue
+
+word_relay()
+
+
+        
