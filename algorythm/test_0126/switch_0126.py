08beacb605b53316a74633dcb38d12523b37bacf
flowerdonk <dbwls5140@gmail.com>
2023-01-26 16:12:53 +0900

    스위치 문제(미완성)

--- algorythm/test_0126/switch_0126.py
+++ algorythm/test_0126/switch_0126.py
@@ -1,10 +1,20 @@
+def Woman(num, switches):
+    changed_switches = switches[:]
+
+    return changed_switches
+
+def Man(num):
+    changed_switches = switches[:]
+
+    return changed_switches
+
+
 num = int(input())
 switches = list(map(int, input().split()))
+member = int(input())
+member_choice = list(map(int, input().split()))
 
-def Woman():
-
-    return 
-
-def Man():
-
-    return
+if member_choice[0] == 1:
+    print(Man(member_choice[1], switches))
+else:
+    print(Woman(member_choice[1], switches))
