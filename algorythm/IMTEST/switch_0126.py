a5bdd92145f4439b5fdc1fd7b249c1c3913e7a69
flowerdonk <dbwls5140@gmail.com>
2023-01-27 17:51:04 +0900

    switch 미완성

--- algorythm/IMTEST/switch_0126.py
+++ algorythm/IMTEST/switch_0126.py
@@ -1,20 +1,31 @@
-def Woman(num, switches):
-    changed_switches = switches[:]
+def Turn(num):
+    if switches[num-1] == 0:
+        switches[num-1] = 1
+    else:
+        switches[num-1] = 0
+    return
 
-    return changed_switches
-
-def Man(num):
-    changed_switches = switches[:]
-
-    return changed_switches
-
-
-num = int(input())
+total = int(input())
 switches = list(map(int, input().split()))
 member = int(input())
-member_choice = list(map(int, input().split()))
 
-if member_choice[0] == 1:
-    print(Man(member_choice[1], switches))
-else:
-    print(Woman(member_choice[1], switches))
+for i in range(member):
+    x, y = map(int, input().split())
+    
+    if x == 1:
+        Turn(y)
+        Turn(y * 2)
+    else:
+        Turn(y)
+        for i in range(total // 2):
+            if y + i > total or y - i < 1 : break
+            if switches[y - 2 - i] == switches[y + i]:
+                Turn(y - 1 - i)
+                Turn(y + 1 + i)
+            else:
+                break
+
+for i in range(1, total+1):
+    print(switches[i], end = ' ')
+    if i % 20 == 0:
+        print()
