5ca4e1394e3329e0e5a16b02a1a412c58a3d7307
flowerdonk <dbwls5140@naver.com>
2023-01-30 20:53:04 +0900

    창고 다각형

--- algorythm/IMTEST/BJ_2304.py
+++ algorythm/IMTEST/BJ_2304.py
@@ -1,21 +1,21 @@
-pillarN = int(input())
-store = [] * 1000
-input_pillar = []
+pillarN = int(input()) # 기둥 갯수
+store = [] * 1000 # 창고
+input_pillar = [] # 기둥 L, H 리스트 저장 - 2차원 리스트
 for i in range(pillarN):
     input_pillar.append(list(map(int, input().split())))
 
-max_L = 0
-max_H = 0
-max_H_idx = 0
-for i in range(pillarN):
+max_L = 0 # 최대 위치
+max_H = 0 # 최대 높이 
+max_H_idx = 0 # 최대 높이 인덱스
+for i in range(pillarN): # 최대 위치 찾기
     max_L = max(max_L, input_pillar[i][0])
 
-for i in range(pillarN):
+for i in range(pillarN): # 최대 높이, 인덱스 찾기
     if max_H <= input_pillar[i][1]:
         max_H = input_pillar[i][1]
         max_H_idx = input_pillar[i][0]
 
-for i in range(1, max_L + 1):
+for i in range(1, max_L + 1): # 창고 리스트에 기둥 정보 담기
     count = 0
     for r in range(pillarN):
         if input_pillar[r][0] == i:
@@ -28,7 +28,7 @@ for i in range(1, max_L + 1):
 
 area = 0
 big = 0
-for i in range(max_H_idx):
+for i in range(max_H_idx): # 최대 높이까지
     if i < max_H_idx:
         if big < store[i]:
             big = store[i]
@@ -37,7 +37,7 @@ for i in range(max_H_idx):
         area += max_H
     
 big = 0
-for i in range(max_L - max_H_idx):
+for i in range(max_L - max_H_idx): # 최대 높이 이후, 역순 계산
     if big < store[max_L - 1 - i]:
         big = store[max_L - 1 - i]
     area += big
