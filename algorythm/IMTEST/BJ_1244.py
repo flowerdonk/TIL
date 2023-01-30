cf59d41d9a6eb8780f429e5893d5c6dc71573b2c
flowerdonk <dbwls5140@gmail.com>
2023-01-30 17:30:21 +0900

    스위치 켜고 끄기

--- algorythm/IMTEST/BJ_1244.py
+++ algorythm/IMTEST/BJ_1244.py
@@ -1,4 +1,4 @@
-def Turn(num):
+def Turn(num): # 스위치 키고 끄는 함수
     if switches[num] == 0:
         switches[num] = 1
     else:
@@ -6,28 +6,28 @@ def Turn(num):
     return
 
 total = int(input())
-switches = [-1] + list(map(int, input().split()))
+switches = [-1] + list(map(int, input().split())) # 0 인덱스 채우기
 member = int(input())
 
 for i in range(member):
-    x, y = map(int, input().split())
-    if not 1 <= y <= total:
+    x, y = map(int, input().split()) # 성별, 위치
+    if not 1 <= y <= total: # 위치가 범위 내 인지 확인
         break
 
-    if x == 1:
+    if x == 1: # 남자일 경우
         Turn(y)
         Turn(y * 2)
-    else:
+    else: # 여자일 경우
         Turn(y)
-        for i in range(total // 2):
-            if y + i > total or y - i < 1 : break
-            if switches[y - 1 - i] == switches[y + 1 + i]:
-                Turn(y - 1 - i)
-                Turn(y + 1 + i)
+        for i in range(total // 2): # 최대 전체의 절반
+            if y + i > total or y - i < 1 : break # 범위 벗어나면 break
+            if switches[y - 1 - i] == switches[y + 1 + i]: # 앞, 뒤 비교
+                Turn(y - 1 - i) # 같으면 변경
+                Turn(y + 1 + i) # 같으면 변경
             else:
-                break
+                break # 같지 않을 때 break
 
-for i in range(1, total+1):
+for i in range(1, total+1): # 출력
     print(switches[i], end = ' ')
-    if i % 20 == 0:
+    if i % 20 == 0: # 20개 까지
         print()
