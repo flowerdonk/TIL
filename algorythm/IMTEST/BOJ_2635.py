11f1da724513c82cec20c357125d40f324adca00
flowerdonk <dbwls5140@gmail.com>
2023-02-02 17:38:26 +0900

    숫자 차이

---
+++ algorythm/IMTEST/BOJ_2635.py
@@ -0,0 +1,17 @@
+def Numbers(first_num):
+    num_list = []
+    max_count = 0
+
+    while num > 0:
+        for i in range(1, first_num//2):
+            num //= 2
+            if num + i
+
+
+
+    return num_list
+
+first_num = int(input())
+num = Numbers(first_num)
+print(len(num))
+print(num, end = ' ')
