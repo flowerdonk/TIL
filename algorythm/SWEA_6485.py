4b63566a630cab24c9556aee10bc286ae9e12041
flowerdonk <dbwls5140@gmail.com>
2023-02-03 14:42:45 +0900

    버스 정류장 정차 횟수 구하기

--- algorythm/SWEA_6485.py
+++ algorythm/SWEA_6485.py
@@ -1,26 +1,14 @@
-import sys
-sys.stdin = open('input.txt')
-
-def Bus(T, N, lineA, P, lineN):
+def Bus(N, lineA, P, lineN):
     count = [0] * 5001
-
+    result = []
     for i in range(N):
         for j in range(lineA[i][0] - 1, lineA[i][1]):
             count[j] += 1
 
-    return count[:P]
+    for i in range(P):
+        result.append(count[lineN[i] - 1])
+    return result
 
-'''
-입력
-
-T 테스트 케이스 수
-N 버스 노선 개수
-Ai Bi 버스 노선 번호 범위 (Ai <= bus <= Bi)
-...
-P 버스 정류장 개수
-Cj j번째 정수는 Cj번 버스 정류장을 지나는 버스 노선의 개수
-...
-'''
 T = int(input())
 result = []
 for i in range(T):
@@ -32,13 +20,8 @@ for i in range(T):
     lineN = []
     for i in range(P):
         lineN.append(int(input()))
-    result.append(Bus(T, N, lineA, P, lineN))
-    
+    result.append(Bus(N, lineA, P, lineN))
 
-'''
-출력
-
-각 정류장에 몇 개의 버스 노선이 다니는지
-'''
 for i in range(T):
     print(f'#{i + 1}', *result[i])
+
