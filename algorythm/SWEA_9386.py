823f7c11e418aa4fef6dcd71c0e7cdcc21596485
flowerdonk <dbwls5140@gmail.com>
2023-02-03 11:57:02 +0900

    0, 1로 이루어진 수열에서 연속한 1의 최대값

--- algorythm/SWEA_9386.py
+++ algorythm/SWEA_9386.py
@@ -1,3 +1,6 @@
+import sys
+sys.stdin = open('input.txt')
+'''
 T = int(input())
 for test_case in range(1, T + 1):
     N = int(input())
@@ -14,3 +17,27 @@ for test_case in range(1, T + 1):
                 ans = cnt
 
     print(f'#{test_case}', ans)
+'''
+def MaxTimes(N, Seque):
+    times = 0
+    max = 0
+    for i in range(N):
+        if Seque[i] == 1:
+            times += 1
+            if max <= times:
+                max = times
+        else:
+            times = 0
+
+    return max
+
+T = int(input())
+N = []
+Seque = []
+
+for i in range(T):
+    N.append(int(input()))
+    Seque.append(list(map(int, list(input()))))
+
+for i in range(T):
+    print(f'#{i + 1} {MaxTimes(N[i], Seque[i])}')
