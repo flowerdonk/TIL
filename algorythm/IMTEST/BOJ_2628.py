469f9e4e84da419fbcf25a9d37b3073fb15bcd6e
flowerdonk <dbwls5140@gmail.com>
2023-02-03 15:14:38 +0900

    종이자르기

--- algorythm/IMTEST/BOJ_2628.py
+++ algorythm/IMTEST/BOJ_2628.py
@@ -1,6 +1,6 @@
 def Cut(x, y, cutN, cut_list):
-    cut_x = [] # [4]
-    cut_y = [] # [3, 2]
+    cut_x = [0] # [4]
+    cut_y = [0] # [3, 2]
 
     for i in range(cutN): # 자르는 값들 x, y 끼리 리스트에 넣기
         if cut_list[i][0] == 0: # 가로로 자를 때
@@ -11,20 +11,22 @@ def Cut(x, y, cutN, cut_list):
     cut_x.sort(reverse=True) # 내림차순 정렬
     cut_y.sort(reverse=True)
 
-    # 가장 긴 길이 초기값 = 양 끝에 자르기 직전까지 거리 중 긴 것
-    big_x = x - cut_x[0] if x - cut_x[0] >= cut_x[-1] else cut_x[-1]
-    big_y = y - cut_y[0] if y - cut_y[0] >= cut_y[-1] else cut_y[-1]
+    if cut_x and cut_y:
+        # 가장 긴 길이 초기값 = 양 끝에 자르기 직전까지 거리 중 긴 것
+        big_x = x - cut_x[0] if x - cut_x[0] >= cut_x[-1] else cut_x[-1]
+        big_y = y - cut_y[0] if y - cut_y[0] >= cut_y[-1] else cut_y[-1]
 
-    # 자르는 선 사이의 거리 중 긴 것 찾기
-    for i in range(len(cut_x) - 1):
-        if big_x <= cut_x[i] - cut_x[i + 1]:
-            big_x = cut_x[i] - cut_x[i + 1]
+        # 자르는 선 사이의 거리 중 긴 것 찾기
+        for i in range(len(cut_x) - 1):
+            if big_x <= cut_x[i] - cut_x[i + 1]:
+                big_x = cut_x[i] - cut_x[i + 1]
 
-    for i in range(len(cut_y) - 1):
-        if big_y <= cut_y[i] - cut_y[i + 1]:
-            big_y = cut_y[i] - cut_y[i + 1]
+        for i in range(len(cut_y) - 1):
+            if big_y <= cut_y[i] - cut_y[i + 1]:
+                big_y = cut_y[i] - cut_y[i + 1]
 
-    return big_x * big_y
+        return big_x * big_y
+    return x*y
 
 x, y = map(int, input().split()) # 전체 종이 가로, 세로
 cutN = int(input()) # 자를 점선의 개수
