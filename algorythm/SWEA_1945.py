e698262209da4e90b1c87366f00ad6aa27d1f885
flowerdonk <dbwls5140@gmail.com>
2023-02-03 11:39:29 +0900

    소인수 지수 구하기

--- algorythm/SWEA_1945.py
+++ algorythm/SWEA_1945.py
@@ -1,3 +1,7 @@
+import sys
+sys.stdin = open('input.txt')
+
+'''
 divs = [2, 3, 5, 7, 11]
 T = int(input())
 
@@ -12,3 +16,32 @@ for test_case in range(1, T + 1):
 
 
     print(f'#{test_case}', *cnts)
+'''
+def Prime(Number):
+    factors = [2, 3, 5, 7, 11]
+    expos = [0] * 5
+    idx = 0
+    while Number > 1:
+        if Number % factors[idx] == 0:
+            expos[idx] += 1
+            Number //= factors[idx]
+            continue
+        idx += 1
+
+    return expos
+
+# input
+T = int(input())
+Numbers =[]
+for i in range(T):
+    Numbers.append(int(input()))
+
+for i in range(T):
+    result = Prime(Numbers[i])
+    print(f'#{i + 1}', *result)
+
+
+
+
+
+
