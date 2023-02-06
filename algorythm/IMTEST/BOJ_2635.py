01d85de3df816f45d11a9514241dd987d54664b6
flowerdonk <dbwls5140@naver.com>
2023-02-06 21:56:09 +0900

    미완

--- algorythm/IMTEST/BOJ_2635.py
+++ algorythm/IMTEST/BOJ_2635.py
@@ -1,17 +1,28 @@
 def Numbers(first_num):
-    num_list = []
+    max_list = []
     max_count = 0
 
-    while num > 0:
-        for i in range(1, first_num//2):
-            num //= 2
-            if num + i
+    for i in range(1, first_num + 1):
+        num_list = [first_num]
+        num = first_num//2
+        num += i
+        count = 0
 
+        while num >= 0:
+            num_list.append(num)
+            num = num_list[count] - num
+            if num < 0:
+                break
+            count += 1
+        
+        if max_count <= len(num_list):
+            max_count = len(num_list)
+            max_list = num_list[:]
 
-
-    return num_list
+    return max_list
 
 first_num = int(input())
 num = Numbers(first_num)
 print(len(num))
-print(num, end = ' ')
+for i in range(len(num)):
+    print(num[i], end = ' ')
