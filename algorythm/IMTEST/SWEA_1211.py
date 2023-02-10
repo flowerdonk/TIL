0b9f89e8daba8117fea3225f09eceecb7ada6858
flowerdonk <dbwls5140@gmail.com>
2023-02-10 17:39:19 +0900

    사다리 최단거리 찾기

--- algorythm/IMTEST/SWEA_1211.py
+++ algorythm/IMTEST/SWEA_1211.py
@@ -1,60 +1,7 @@
 import sys
 sys.stdin = open('input.txt')
-#
-# def Ladder(arr, L):
-    # firstLine = []
-    # for x in range(len(arr[0])):
-    #     if arr[0][x] == 1:
-    #         firstLine.append(x)  # 시작점 좌표 담기
-    #
-    # keep_x = keep_y = 0
-    # minN = L * L
-    # minX = 0
-    #
-    # for start in firstLine:  # 시작점 하나씩 반복
-    #     keep_x = start  # 시작점 저장
-    #     y = 0
-    #     x = firstLine.index(start)  # 시작점 x 인덱스
-    #     cnt = 0  # 꺾은 수
-    #
-    #     while x <= len(firstLine) - 1:
-    #         if y == L - 1:  # 마지막 줄
-    #             if L + cnt < minN:
-    #                 minX = start
-    #                 minN = L + cnt
-    #                 print(minN)
-    #                 print(minX)
-    #                 # y = 0
-    #             break
-    #
-    #
-    #         else:
-    #             if x != len(firstLine) - 1 and arr[y][firstLine[x] + 1] == 1:  # 오른쪽 길
-    #                 keep_y = y  # y좌표 저장
-    #                 x += 1  # x 전 값으로
-    #                 y += 1
-    #                 cnt += firstLine[x] - firstLine[x - 1]
-    #                 continue  # 다음 x좌표로
-    #             elif x != 0 and arr[y][firstLine[x] - 1] == 1:  # 왼쪽 길
-    #                 keep_y = y  # y좌표 저장
-    #                 x -= 1
-    #                 y += 1
-    #                 cnt += firstLine[x + 1] - firstLine[x]
-    #                 continue
-    #             else:
-    #                 y += 1
-#
-#
-#     return minX
-#
-#
-# L = 100
-# T = 10
-# for i in range(1, T + 1):
-#     N = int(input())
-#     arr = [list(map(int, input().split())) for _ in range(L)]
-#     print(f'#{N} {Ladder(arr, L)}')
 
+## 교수님 풀이
 T = 10
 for i in range(1, T + 1):
     _ = int(input())
@@ -65,8 +12,10 @@ for i in range(1, T + 1):
         si = 0
         if arr[si][sj] != 1:
             continue
+
         cnt, dj = 0, 0
         ci, cj = si, sj
+
         while ci < 99:
             cnt += 1
             if dj == 0:
@@ -81,7 +30,54 @@ for i in range(1, T + 1):
                     dj = 0
                     ci += 1
             cj += dj
+
         if mn >= cnt:
             mn, ans = cnt, sj - 1
 
     print(f'#{i} {ans}')
+
+## 다시 풀이
+def ladder(arr, N):
+    mn = N * N
+
+    for sj in range(1, N + 1):
+        si = 0
+
+        if arr[si][sj] != 1:
+            continue
+
+        cnt = 0
+        dj = 0 # 선을 만나면 좌, 우 이동하는 방향
+        ci, cj = si, sj
+
+        while ci < N - 1:
+            cnt += 1
+            if dj == 0: # 방향 아래
+                if arr[ci][cj - 1] == 1:
+                    dj = -1
+                elif arr[ci][cj + 1] == 1:
+                    dj = 1
+                else:
+                    ci += 1
+            else:
+                if arr[ci][cj + dj] == 0:
+                    dj = 0
+                    ci += 1
+            cj += dj
+
+        if mn >= cnt:
+            mn = cnt
+            ans = sj - 1
+
+    return ans
+
+
+T = 10
+N = 100
+for i in range(1, T + 1):
+    _ = int(input())
+    arr = [[0] + list(map(int, input().split())) + [0] for _ in range(N)]
+    print(f'#{i} {ladder(arr, N)}')
+
+
+
