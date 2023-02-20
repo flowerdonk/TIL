45213e4e3c8505d03aff677960b03c57466b61f2
flowerdonk <dbwls5140@gmail.com>
2023-02-20 17:38:23 +0900

    암호 생성기

---
+++ algorythm/SWEA_1225.py
@@ -0,0 +1,24 @@
+import sys
+sys.stdin = open('input.txt')
+
+def code(arr):
+    cnt = 1
+
+    while cnt: # 0 이하 발생 시 반복문 종료
+        for n in range(1, 6): # 5씩 싸이클
+            fst = arr.pop(0) # 맨 앞 변수에 할당
+
+            if fst - n <= 0: # 감소한 수가 0 이하일 때
+                arr.append(0) # 0 삽입
+                cnt = 0 # 반복문 종료
+                break
+            else:
+                arr.append(fst - n) # 감소한 수 삽입
+
+T = 10
+for i in range(1, T + 1):
+    _ = input()
+    arr = list(map(int, input().split()))
+    code(arr)
+
+    print(f'#{i}', *arr)
