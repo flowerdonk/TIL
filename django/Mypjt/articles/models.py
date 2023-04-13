f750233a73e91599d2f830aa24a6338875c97aea
flowerdonk <dbwls5140@gmail.com>
2023-04-13 17:36:24 +0900

    0413

---
+++ django/Mypjt/articles/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.conf import settings
+
+class Article(models.Model):
+    title = models.CharField(max_length=20)
+    category = models.CharField(max_length=10, default='')
+    content = models.TextField()
+    image = models.ImageField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.content
