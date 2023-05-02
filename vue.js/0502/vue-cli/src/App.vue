24f843ad655799bce4321a606cba16b8d9ab9d74
flowerdonk <dbwls5140@gmail.com>
2023-05-02 17:55:25 +0900

    0502

---
+++ vue.js/0502/vue-cli/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <MyComponent/> <!-- 3. 사용하기 -->
+    <MyComponent/>
+    <MyComponent/>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+// 1. 불러오기
+import MyComponent from './components/MyComponent.vue'
+/*
+1) import MyComponent from '@/components/MyComponent'
+  -> 동일 기능, @은 src/ 의미, .vue 생략 가능
+2) MyComponent는 변수, 따라서 해당 파일 내에서 다른 이름으로 임의 설정 가능
+*/
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    MyComponent, // 2. 등록하기
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue.js/0502/vue-cli/src/components/MyComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="border">
+    <h1>This is MyComponent</h1>
+    <MyChild/> <!-- 3. 사용하기 -->
+    <MyChild/>
+    <MyChild/>
+    <MyChild/>
+  </div>
+  <!-- <div> </div> 생성 불가, 루트는 하나만 가능 -->
+</template>
+
+<script>
+import MyChild from '@/components/MyChild' // 1. 불러오기, MyComponent 안에 MyChild 넣기
+
+export default {
+  name: 'MyComponent', // 컴포넌트명
+  components : { 
+    MyChild, // 2. 등록하기
+  }
+}
+</script>
+
+<style>
+.border {
+  border: solid;
+}
+</style>
