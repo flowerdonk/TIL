118a42ecb79bf01d7a34641190f9b1e472f00332
flowerdonk <dbwls5140@gmail.com>
2023-07-16 22:24:01 +0900

    spring api practice

---
+++ spring/api/src/main/java/com/example/api/controller/PutController.java
@@ -0,0 +1,48 @@
+package com.example.api.controller;
+
+import com.example.api.dto.MemberDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/put-api")
+public class PutController {
+
+    // http://localhost:8080/api/v1/put-api/member
+    @PutMapping(value = "/member")
+    public String postMember(@RequestBody Map<String, Object> putData) {
+        StringBuilder sb = new StringBuilder();
+
+        putData.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+
+        return sb.toString();
+    }
+
+    // http://localhost:8080/api/v1/put-api/member1
+    @PutMapping(value = "/member1")
+    public String postMemberDto1(@RequestBody MemberDto memberDto) {
+        return memberDto.toString();
+    }
+
+    // http://localhost:8080/api/v1/put-api/member2
+    // JSON 형태로 응답, @RestController 클래스는 @ResponseBody 생략 가능(자동으로 값을 JSON 형식으로 변환)
+    @PutMapping(value = "/member2")
+    public MemberDto postMemberDto2(@RequestBody MemberDto memberDto) {
+        return memberDto;
+    }
+
+    // http://localhost:8080/api/v1/put-api/member3
+    // ResponseEntity 활용
+    @PutMapping(value = "/member3")
+    public ResponseEntity<MemberDto> postMemberDto3(@RequestBody MemberDto memberDto) {
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(memberDto);
+    }
+}
